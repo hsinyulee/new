c958c02f92171e3afdb4092c6b77ec3d32a13519
hsinyulee <kngngop@gmail.com>
2015-12-21 09:57:56 +0800

    123

---
+++ src/ex6.java
@@ -0,0 +1,20 @@
+
+public class ex6 {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+	class Question {
+		   String description;
+		   public Question(String desc) {
+		      this.description = desc;
+		   }
+		   public void print() {
+		      System.out.println(desc);
+		   }      
+		}
+}
